528173aa8c3189afbcc0ad9cd68410c50ca244da
Azureun <112467837+azureun@users.noreply.github.com>
2024-08-13 15:28:20 +0900

    Add files via upload

---
+++ #01 Basic/app.js
@@ -0,0 +1,29 @@
+//Variables(let, const, var)
+
+/*
+//var은 언제든지 업데이트 가능한 변수 - 실수로 업데이트 해도 오류 뜨지 않음.
+//기본적으로 const, 필요 시 let, 사용금지는 var
+
+const a = 5, b = 2;     //상수 선언 - 값 변경 불가능
+const myName1 = "kazu";
+const veryLongVarName = 0;      //camelCase식으로 변수 선언
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName1);
+
+//const and let
+let myName2 = "kazu";    //변수 선언 - 값 변경 가능
+myName2 = 'azureun';
+
+console.log("your new name is " + myName2);
+*/
+
+//Boolean(true/false)
+
+const amIFat = true;
+let something;
+console.log(something);
+
+
