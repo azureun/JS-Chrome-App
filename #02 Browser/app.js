a6beab7693e5cfdf6b7f67b1d262535805d9a757
Azureun <112467837+azureun@users.noreply.github.com>
2024-08-19 22:16:46 +0900

    Add files via upload

---
+++ #02 Browser/app.js
@@ -0,0 +1,12 @@
+//JS는 이미 html과 연결되어 있다.
+
+//index.html에서 F12, Console 들어가서 document.title="제목" 적으면 자동으로 제목 내용으로 변경됨.
+document.title=document.getElementById("title");    //document는 website를 의미함.
+
+//document.location
+//object들 어디에 위치하는지 띄워줌.
+
+//console.dir()
+//element를 더 자세하게 보여줌.
+console.dir(title);
+
